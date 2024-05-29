14d9263f57dd4b11d3e34e01536f603f351bebee
rajesh <rajesh.r@vbridgehub.com>
2024-05-29 17:50:55 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 import Chitplans from "./components/chitplans/Chitplans";
-import Generalinfo from "./components/Generalinfo/Generalinfo"
+import Generalinfo from "./components/Generalinfo/Generalinfo";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 
